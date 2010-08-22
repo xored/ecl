84f866eb2aa2a50acd4ce1e80567765ec5a74bb4
alena <alena>
2010-08-22 14:17:48 +0000

    added implementation

--- org.eclipse.ecl.core/src/org/eclipse/ecl/internal/commands/ForeachService.java
+++ org.eclipse.ecl.core/src/org/eclipse/ecl/internal/commands/ForeachService.java
@@ -2,15 +2,34 @@ package org.eclipse.ecl.internal.commands;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ecl.core.Command;
+import org.eclipse.ecl.core.Foreach;
 import org.eclipse.ecl.runtime.ICommandService;
+import org.eclipse.ecl.runtime.IPipe;
 import org.eclipse.ecl.runtime.IProcess;
+import org.eclipse.ecl.runtime.ISession;
+import org.eclipse.emf.ecore.EObject;
 
 public class ForeachService implements ICommandService {
 
 	public IStatus service(Command command, IProcess context)
 			throws InterruptedException, CoreException {
-		return null;
+		Foreach foreach = (Foreach) command;
+		IStatus status = Status.OK_STATUS;
+		for (EObject o : foreach.getInput()) {
+			ISession session = context.getSession();
+			IPipe in = session.createPipe();
+			in.write(o);
+			in.close(Status.OK_STATUS);
+
+			Command doCommand = foreach.getDo();
+			status = session.execute(doCommand, in, null).waitFor();
+			if (status.getSeverity() != IStatus.OK) {
+				break;
+			}
+		}
+		return status;
 	}
 
 }
