6c6487d21060cc4ed3cf1c74259424aecc45a005
haiodo <haiodo>
2010-09-29 05:20:52 +0000

    * correct stream manipulations

--- org.eclipse.ecl.core/src/org/eclipse/ecl/internal/core/EMFStreamPipe.java
+++ org.eclipse.ecl.core/src/org/eclipse/ecl/internal/core/EMFStreamPipe.java
@@ -1,5 +1,9 @@
 package org.eclipse.ecl.internal.core;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -14,12 +18,12 @@ import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl;
 
 public class EMFStreamPipe implements IPipe {
 
-	private final InputStream in;
-	private final OutputStream out;
+	private final DataInputStream in;
+	private final DataOutputStream out;
 
 	public EMFStreamPipe(InputStream in, OutputStream out) {
-		this.in = in;
-		this.out = out;
+		this.in = new DataInputStream(in);
+		this.out = new DataOutputStream(out);
 	}
 
 	public IPipe close(IStatus status) throws CoreException {
@@ -43,7 +47,11 @@ public class EMFStreamPipe implements IPipe {
 	public Object take(long timeout) throws CoreException {
 		Resource r = new BinaryResourceImpl();
 		try {
-			r.load(in, null);
+			int size = in.readInt();
+			byte[] data = new byte[size];
+			in.readFully(data);
+			ByteArrayInputStream bin = new ByteArrayInputStream(data);
+			r.load(bin, null);
 			return r.getContents().get(0);
 		} catch (IOException e) {
 			throw new CoreException(new Status(IStatus.ERROR,
@@ -55,12 +63,14 @@ public class EMFStreamPipe implements IPipe {
 		Resource r = new BinaryResourceImpl();
 		r.getContents().add((EObject) object);
 		try {
-			r.save(out, null);
+			ByteArrayOutputStream bout = new ByteArrayOutputStream();
+			r.save(bout, null);
+			out.writeInt(bout.size());
+			out.write(bout.toByteArray());
 		} catch (IOException e) {
 			throw new CoreException(new Status(IStatus.ERROR,
 					CorePlugin.PLUGIN_ID, e.getMessage(), e));
 		}
 		return this;
 	}
-
 }
