946cb8934e1549e5f59e839ad7b9633098c7c0c3
alena <alena>
2010-10-14 07:05:12 +0000

    New methods

--- org.eclipse.ecl.core/src/org/eclipse/ecl/core/util/ScriptletFactory.java
+++ org.eclipse.ecl.core/src/org/eclipse/ecl/core/util/ScriptletFactory.java
@@ -4,8 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.ecl.core.Binding;
+import org.eclipse.ecl.core.Block;
 import org.eclipse.ecl.core.Command;
 import org.eclipse.ecl.core.CoreFactory;
+import org.eclipse.ecl.core.Exec;
+import org.eclipse.ecl.core.ExecutableParameter;
+import org.eclipse.ecl.core.LiteralParameter;
+import org.eclipse.ecl.core.Parallel;
+import org.eclipse.ecl.core.Parameter;
 import org.eclipse.ecl.core.Pipeline;
 import org.eclipse.ecl.core.Sequence;
 import org.eclipse.ecl.core.With;
@@ -31,9 +37,7 @@ public class ScriptletFactory {
 	}
 
 	public static Sequence makeSeq(List<Command> commands) {
-		Sequence sequence = CoreFactory.eINSTANCE.createSequence();
-		sequence.getCommands().addAll(commands);
-		return sequence;
+		return block(CoreFactory.eINSTANCE.createSequence(), commands);
 	}
 
 	public static Pipeline makePipe(Command... commands) {
@@ -41,12 +45,23 @@ public class ScriptletFactory {
 	}
 
 	public static Pipeline makePipe(List<Command> commands) {
-		Pipeline pipeline = CoreFactory.eINSTANCE.createPipeline();
+		return block(CoreFactory.eINSTANCE.createPipeline(), commands);
+	}
+
+	public static Parallel makeParallel(Command... commands) {
+		return makeParallel(Arrays.asList(commands));
+	}
+
+	public static Parallel makeParallel(List<Command> commands) {
+		return block(CoreFactory.eINSTANCE.createParallel(), commands);
+	}
+
+	private static <T extends Block> T block(T block, List<Command> commands) {
 		for (Command command : commands) {
 			if (command != null)
-				addToPipe(pipeline, command);
+				block.getCommands().add(command);
 		}
-		return pipeline;
+		return block;
 	}
 
 	public static With makeWith(Command withObject, Command doObject) {
@@ -56,17 +71,35 @@ public class ScriptletFactory {
 		return with;
 	}
 
-	private static void addToPipe(Pipeline pipeline, Command command) {
-		if (command == null)
-			return;
-		if (command instanceof Pipeline) {
-			for (Command inner : ((Pipeline) command).getCommands()) {
-				addToPipe(pipeline, inner);
-			}
-		} else if (command instanceof Sequence) {
-			throw new RuntimeException("Cannot add sequence in pipeline");
-		} else {
-			pipeline.getCommands().add(command);
+	public static Exec makeExec(String name, Parameter... parameters) {
+		return makeExec(null, name, parameters);
+	}
+
+	public static Exec makeExec(String ns, String name, Parameter... parameters) {
+		Exec e = CoreFactory.eINSTANCE.createExec();
+		e.setNamespace(ns);
+		e.setName(name);
+		for (Parameter p : parameters) {
+			if (p != null)
+				e.getParameters().add(p);
 		}
+		return e;
+	}
+
+	public static LiteralParameter makeLiteralParameter(String name,
+			String value) {
+		LiteralParameter p = CoreFactory.eINSTANCE.createLiteralParameter();
+		p.setName(name);
+		p.setLiteral(value);
+		return p;
+	}
+
+	public static ExecutableParameter makeExecutableParameter(String name,
+			Command value) {
+		ExecutableParameter p = CoreFactory.eINSTANCE
+				.createExecutableParameter();
+		p.setName(name);
+		p.setCommand(value);
+		return p;
 	}
 }
