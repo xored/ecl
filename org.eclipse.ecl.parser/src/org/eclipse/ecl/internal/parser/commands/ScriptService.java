7ab2fabedfde19af9d15b91a0728f14f4b863c80
alena <alena>
2010-10-31 12:48:01 +0000

    + script service

---
+++ org.eclipse.ecl.parser/src/org/eclipse/ecl/internal/parser/commands/ScriptService.java
@@ -0,0 +1,33 @@
+package org.eclipse.ecl.internal.parser.commands;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ecl.core.Command;
+import org.eclipse.ecl.core.Script;
+import org.eclipse.ecl.internal.core.CorePlugin;
+import org.eclipse.ecl.parser.EclCoreParser;
+import org.eclipse.ecl.runtime.EclCompiler;
+import org.eclipse.ecl.runtime.EclRuntime;
+import org.eclipse.ecl.runtime.ICommandService;
+import org.eclipse.ecl.runtime.IProcess;
+
+public class ScriptService implements ICommandService {
+
+	public IStatus service(Command command, IProcess context)
+			throws InterruptedException, CoreException {
+		Script s = (Script) command;
+		String content = s.getContent();
+		if (content == null || content.length() == 0)
+			return Status.OK_STATUS;
+		Command c = EclCoreParser.newCommand(content);
+		if (c == null) {
+			return new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID,
+					"Errors in the script.");
+		}
+		c = EclCompiler.compile(c);
+		IProcess process = EclRuntime.createSession().execute(c);
+		return process.waitFor();
+	}
+
+}
