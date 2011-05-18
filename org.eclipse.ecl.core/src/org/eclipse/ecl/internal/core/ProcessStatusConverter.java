f53c192b935a9ee3f7aee6f7272b2c36ab96efe4
alena <alena>
2011-05-18 06:09:28 +0000

    Send information about exception in status message

--- org.eclipse.ecl.core/src/org/eclipse/ecl/internal/core/ProcessStatusConverter.java
+++ org.eclipse.ecl.core/src/org/eclipse/ecl/internal/core/ProcessStatusConverter.java
@@ -1,5 +1,8 @@
 package org.eclipse.ecl.internal.core;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ecl.core.CoreFactory;
 import org.eclipse.ecl.core.CorePackage;
@@ -26,7 +29,21 @@ public class ProcessStatusConverter implements
 	public ProcessStatus toEObject(Status status) {
 		ProcessStatus ps = CoreFactory.eINSTANCE.createProcessStatus();
 		ps.setCode(status.getCode());
-		ps.setMessage(status.getMessage());
+		String msg = status.getMessage();
+		StringBuilder builder = new StringBuilder();
+		if (msg != null && msg.length() > 0) {
+			builder.append(msg);
+		}
+		if (status.getException() != null) {
+			if (builder.length() > 0) {
+				builder.append("\n");
+			}
+			StringWriter out = new StringWriter();
+			status.getException().printStackTrace(new PrintWriter(out));
+			String stackTrace = out.toString();
+			builder.append(stackTrace);
+		}
+		ps.setMessage(builder.toString());
 		ps.setPluginId(status.getPlugin());
 		ps.setSeverity(status.getSeverity());
 		return ps;
