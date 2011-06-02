90cd0459d57be6d1624e40abaea447a282b0caf9
alena <alena@192.168.2.159>
2011-06-02 19:27:29 +0700

    Initial implementation of ECL validator

---
+++ org.eclipse.ecl.parser/src/org/eclipse/ecl/parser/EclValidator.java
@@ -0,0 +1,38 @@
+package org.eclipse.ecl.parser;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ecl.core.Command;
+import org.eclipse.ecl.core.Exec;
+import org.eclipse.ecl.runtime.EclCompiler;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+
+public enum EclValidator {
+
+	Instanse;
+
+	public IStatus validate(String content) {
+		try {
+			Command raw = EclCoreParser.newCommand(content);
+			if (raw instanceof Exec) {
+				validate((Exec) raw);
+			}
+		} catch (CoreException e) {
+			return e.getStatus();
+		}
+		return Status.OK_STATUS;
+	}
+
+	private IStatus validate(Exec exec) throws CoreException {
+		Command command = EclCompiler.compile(exec);
+		EList<EObject> eContents = command.eContents();
+		for (EObject e : eContents) {
+			if (e instanceof Exec) {
+				validate((Exec) e);
+			}
+		}
+		return Status.OK_STATUS;
+	}
+}
