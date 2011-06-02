113728b2e9f22a8b2d1a836c47430c7d7040bc32
alena <alena@192.168.2.159>
2011-06-02 19:26:23 +0700

    Allow to specify both message and status during construction

--- org.eclipse.ecl.parser/src/org/eclipse/ecl/parser/ScriptErrorStatus.java
+++ org.eclipse.ecl.parser/src/org/eclipse/ecl/parser/ScriptErrorStatus.java
@@ -12,11 +12,16 @@ public class ScriptErrorStatus extends Status {
 
 	public ScriptErrorStatus(int severity, String pluginId, String message,
 			int line, int column, int length) {
+		this(severity, pluginId, message, line, column, length, null);
+	}
+
+	public ScriptErrorStatus(int severity, String pluginId, String message,
+			int line, int column, int length, IStatus cause) {
 		super(severity, pluginId, message);
 		this.line = line;
 		this.column = column;
 		this.length = length;
-		this.cause = null;
+		this.cause = cause;
 	}
 
 	public ScriptErrorStatus(IStatus cause, int line, int column, int length) {
