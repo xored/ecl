b3ad896e1e96b0eeb6b742cf18e815db414ae42e
Yuri Strot <ystrot@gmail.com>
2011-12-23 17:11:40 +0700

    Add missed copyrights.

--- plugins/org.eclipse.ecl.debug.core/src/org/eclipse/ecl/internal/debug/core/Plugin.java
+++ plugins/org.eclipse.ecl.debug.core/src/org/eclipse/ecl/internal/debug/core/Plugin.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 xored software, Inc.  
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and Implementation (Yuri Strot)
+ *******************************************************************************/
 package org.eclipse.ecl.internal.debug.core;
 
 import org.eclipse.core.runtime.ILog;
