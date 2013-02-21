1bb270380b33dad1a8de2644b6cdad6d4eacbe0d
Sergey Sitnikov <sergey@xored.com>
2013-02-21 13:13:59 +0700

    invoke args conversion bug fix

--- plugins/org.eclipse.ecl.interop/src/org/eclipse/ecl/interop/internal/commands/InvokeService.java
+++ plugins/org.eclipse.ecl.interop/src/org/eclipse/ecl/interop/internal/commands/InvokeService.java
@@ -22,30 +22,34 @@ public class InvokeService implements ICommandService {
 	private static final Object NOT_CONVERTIBLE = new Object();
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public IStatus service(Command command, IProcess context) throws CoreException {
+	public IStatus service(Command command, IProcess context)
+			throws CoreException {
 
 		final Invoke cmd = (Invoke) command;
 
 		final Object object = cmd.getObject();
-		if (object == null) return error("Null invocation target.");
+		if (object == null)
+			return error("Null invocation target.");
 
 		final Class class_ = object.getClass();
 		final String name = cmd.getName();
-		if (name == null || name.length() == 0) return error("Empty method name.");
+		if (name == null || name.length() == 0)
+			return error("Empty method name.");
 
 		final Object[] args = cmd.getArgs().toArray();
 
 		// --
 
-		if (class_.isArray()) return processArrayMethod(object, name, args,
-				context.getOutput());
+		if (class_.isArray())
+			return processArrayMethod(object, name, args, context.getOutput());
 
 		// --
 
 		Object result = null;
 		try {
 			final Method method = matchMethod(class_, name, args);
-			if (method == null) return error("Method not found.");
+			if (method == null)
+				return error("Method not found.");
 
 			if (object instanceof Widget) {
 				Widget widget = (Widget) object;
@@ -59,50 +63,57 @@ public class InvokeService implements ICommandService {
 
 				widget.getDisplay().syncExec(future);
 				result = future.get();
-			} else result = method.invoke(object, args);
+			} else
+				result = method.invoke(object, args);
 
 		} catch (Exception e) {
-			return error(
-					String.format(
-							"%s: %s", e.getClass().getName(),
-							e.getMessage()
-					)
-			);
+			return error(String.format("%s: %s", e.getClass().getName(),
+					e.getMessage()));
 		}
 
-		if (result != null) context.getOutput().write(result);
+		if (result != null)
+			context.getOutput().write(result);
 		return Status.OK_STATUS;
 	}
 
 	private static IStatus processArrayMethod(Object array, String name,
 			Object[] args, IPipe out) throws CoreException {
 		if (name.equals("get")) {
-			if (args.length != 1) return error("Invalid number of arguments.");
+			if (args.length != 1)
+				return error("Invalid number of arguments.");
 			Object index = convert(int.class, args[0], true, true);
-			if (index == NOT_CONVERTIBLE) return error("Invalid index type.");
+			if (index == NOT_CONVERTIBLE)
+				return error("Invalid index type.");
 
 			Object result = Array.get(array, (Integer) index);
-			if (result != null) out.write(result);
+			if (result != null)
+				out.write(result);
 
 			return Status.OK_STATUS;
 		} else if (name.equals("set")) {
-			if (args.length != 2) return error("Invalid number of arguments.");
+			if (args.length != 2)
+				return error("Invalid number of arguments.");
 
 			Object index = convert(int.class, args[0], true, true);
-			if (index == NOT_CONVERTIBLE) return error("Invalid index type.");
+			if (index == NOT_CONVERTIBLE)
+				return error("Invalid index type.");
 
-			Object value = convert(array.getClass().getComponentType(), args[1], true, true);
-			if (value == NOT_CONVERTIBLE) return error("Invalid value type.");
+			Object value = convert(array.getClass().getComponentType(),
+					args[1], true, true);
+			if (value == NOT_CONVERTIBLE)
+				return error("Invalid value type.");
 
 			Array.set(array, (Integer) index, value);
 
 			return Status.OK_STATUS;
 		} else if (name.equals("length")) {
-			if (args.length != 0) return error("Invalid number of arguments.");
+			if (args.length != 0)
+				return error("Invalid number of arguments.");
 
 			out.write(Array.getLength(array));
 			return Status.OK_STATUS;
-		} else return error("Unknown array pseudo-method name.");
+		} else
+			return error("Unknown array pseudo-method name.");
 	}
 
 	private static Status error(String message) {
@@ -112,32 +123,38 @@ public class InvokeService implements ICommandService {
 	@SuppressWarnings("rawtypes")
 	private static Method matchMethod(Class class_, String name, Object[] args) {
 		Method result = matchMethod(class_, name, args, false, false);
-		if (result == null) result = matchMethod(class_, name, args, true, false);
-		if (result == null) result = matchMethod(class_, name, args, true, true);
+		if (result == null)
+			result = matchMethod(class_, name, args, true, false);
+		if (result == null)
+			result = matchMethod(class_, name, args, true, true);
 		return result;
 	}
 
-	/* TODO:
-	 *
-	 *   1. Null handling.
-	 *   2. Proper overloading resolution.
-	 *
-	 *   Standard says we must select the most "specific" method.
-	 *
-	 *   3. What will happen if arrays passed?
-	 *
-	 *   Useful reading:
-	 *   http://geekexplains.blogspot.com/2009/06/choosing-most-specific-method-tricky.html
+	/*
+	 * TODO:
+	 * 
+	 * 1. Null handling. 2. Proper overloading resolution.
+	 * 
+	 * Standard says we must select the most "specific" method.
+	 * 
+	 * 3. What will happen if arrays passed?
+	 * 
+	 * Useful reading:
+	 * http://geekexplains.blogspot.com/2009/06/choosing-most-specific
+	 * -method-tricky.html
 	 */
 	@SuppressWarnings({ "rawtypes" })
-	private static Method matchMethod(Class class_, String name, Object[] args, boolean doWiden, boolean doNarrow) {
+	private static Method matchMethod(Class class_, String name, Object[] args,
+			boolean doWiden, boolean doNarrow) {
 		final Method[] methods = class_.getMethods();
 
 		for (Method m : methods) {
-			if (!m.getName().equals(name)) continue;
+			if (!m.getName().equals(name))
+				continue;
 
 			final Class[] paramTypes = m.getParameterTypes();
-			if (paramTypes.length != args.length) continue;
+			if (paramTypes.length != args.length)
+				continue;
 
 			boolean done = true;
 			for (int i = 0; i < paramTypes.length; ++i) {
@@ -147,41 +164,47 @@ public class InvokeService implements ICommandService {
 				}
 			}
 
-			if (done) return m;
+			if (done)
+				return m;
 		}
 
 		return null;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static Object convert(Class to, Object value, boolean doWiden, boolean doNarrow) {
+	private static Object convert(Class to, Object value, boolean doWiden,
+			boolean doNarrow) {
 
 		Class from = value.getClass();
 
 		// if Java tells us it is convertible, let it go
-		if (to.isAssignableFrom(from)) return value;
+		if (to.isAssignableFrom(from))
+			return value;
 
 		to = toPrimitive(to);
 		from = toPrimitive(from);
 
-		// always self assignable
-		if (to == from) return value;
-
 		// if one of the types or both are not convertible to primitive,
 		// we can not do anything about it
-		if (to == null || from == null) return NOT_CONVERTIBLE;
+		if (to == null || from == null)
+			return NOT_CONVERTIBLE;
+
+		// always self assignable
+		if (to == from)
+			return value;
 
 		if (doWiden) {
 			// try to widen
 			Object converted = widden(to, value);
-			if (converted != NOT_CONVERTIBLE) return converted;
+			if (converted != NOT_CONVERTIBLE)
+				return converted;
 		}
 
-		if (doNarrow)
-		{
+		if (doNarrow) {
 			// try to narrow
 			Object converted = narrow(to, value);
-			if (converted != NOT_CONVERTIBLE) return converted;
+			if (converted != NOT_CONVERTIBLE)
+				return converted;
 		}
 
 		return NOT_CONVERTIBLE;
@@ -190,33 +213,43 @@ public class InvokeService implements ICommandService {
 	@SuppressWarnings("rawtypes")
 	private static Object widden(Class to, Object value) {
 
-		if (to == value.getClass()) return value;
-		if (to == toPrimitive(value.getClass())) return value;
+		if (to == value.getClass())
+			return value;
+		if (to == toPrimitive(value.getClass()))
+			return value;
 
 		Number number = null;
 		Character character = null;
 
-		if (value instanceof Number) number = (Number) value;
-		if (value instanceof Character) character = (Character) value;
+		if (value instanceof Number)
+			number = (Number) value;
+		if (value instanceof Character)
+			character = (Character) value;
 
 		if (to == long.class) {
-			if (value instanceof Byte || value instanceof Short ||
-					value instanceof Integer) return new Long(number.longValue());
-			else if (value instanceof Character) return new Long(character.charValue());
-			else return NOT_CONVERTIBLE;
+			if (value instanceof Byte || value instanceof Short
+					|| value instanceof Integer)
+				return new Long(number.longValue());
+			else if (value instanceof Character)
+				return new Long(character.charValue());
+			else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == int.class) {
 			if (value instanceof Byte || value instanceof Short)
 				return new Integer(number.intValue());
-			else if (value instanceof Character) return new Integer(character.charValue());
-			else return NOT_CONVERTIBLE;
+			else if (value instanceof Character)
+				return new Integer(character.charValue());
+			else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == short.class) {
 			if (value instanceof Byte)
 				return new Short(number.shortValue());
-			else return NOT_CONVERTIBLE;
+			else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == byte.class) {
@@ -224,20 +257,24 @@ public class InvokeService implements ICommandService {
 		}
 
 		if (to == double.class) {
-			if (value instanceof Byte || value instanceof Short ||
-					value instanceof Integer || value instanceof Long ||
-					value instanceof Float)
+			if (value instanceof Byte || value instanceof Short
+					|| value instanceof Integer || value instanceof Long
+					|| value instanceof Float)
 				return new Double(number.doubleValue());
-			else if (value instanceof Character) return new Double(character.charValue());
-			else return NOT_CONVERTIBLE;
+			else if (value instanceof Character)
+				return new Double(character.charValue());
+			else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == float.class) {
-			if (value instanceof Byte || value instanceof Short ||
-					value instanceof Integer || value instanceof Long)
+			if (value instanceof Byte || value instanceof Short
+					|| value instanceof Integer || value instanceof Long)
 				return new Float(number.floatValue());
-			else if (value instanceof Character) return new Float(character.charValue());
-			else return NOT_CONVERTIBLE;
+			else if (value instanceof Character)
+				return new Float(character.charValue());
+			else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == char.class) {
@@ -249,14 +286,18 @@ public class InvokeService implements ICommandService {
 
 	@SuppressWarnings("rawtypes")
 	private static Object narrow(Class to, Object value) {
-		if (to == value.getClass()) return value;
-		if (to == toPrimitive(value.getClass())) return value;
+		if (to == value.getClass())
+			return value;
+		if (to == toPrimitive(value.getClass()))
+			return value;
 
 		Number number = null;
 		Character character = null;
 
-		if (value instanceof Number) number = (Number) value;
-		if (value instanceof Character) character = (Character) value;
+		if (value instanceof Number)
+			number = (Number) value;
+		if (value instanceof Character)
+			character = (Character) value;
 
 		if (to == long.class) {
 			if (value instanceof Number) {
@@ -264,9 +305,10 @@ public class InvokeService implements ICommandService {
 				double d = number.doubleValue();
 				if (l == d && d >= Long.MIN_VALUE && d <= Long.MAX_VALUE)
 					return new Long(l);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == int.class) {
@@ -275,9 +317,10 @@ public class InvokeService implements ICommandService {
 				double d = number.doubleValue();
 				if (l == d && l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE)
 					return new Integer((int) l);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == short.class) {
@@ -286,15 +329,16 @@ public class InvokeService implements ICommandService {
 				double d = number.doubleValue();
 				if (l == d && l >= Short.MIN_VALUE && l <= Short.MAX_VALUE)
 					return new Short((short) l);
-				else return NOT_CONVERTIBLE;
-			}
-			else if (value instanceof Character) {
+				else
+					return NOT_CONVERTIBLE;
+			} else if (value instanceof Character) {
 				char c = character.charValue();
 				if (c >= Short.MIN_VALUE && c <= Short.MAX_VALUE)
 					return new Short((short) c);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == byte.class) {
@@ -303,15 +347,16 @@ public class InvokeService implements ICommandService {
 				double d = number.doubleValue();
 				if (l == d && l >= Byte.MIN_VALUE && l <= Byte.MAX_VALUE)
 					return new Byte((byte) l);
-				else return NOT_CONVERTIBLE;
-			}
-			else if (value instanceof Character) {
+				else
+					return NOT_CONVERTIBLE;
+			} else if (value instanceof Character) {
 				char c = character.charValue();
 				if (c >= Byte.MIN_VALUE && c <= Byte.MAX_VALUE)
 					return new Byte((byte) c);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == double.class) {
@@ -323,20 +368,23 @@ public class InvokeService implements ICommandService {
 				double d = number.doubleValue();
 				if (d >= Float.MIN_VALUE && d <= Float.MAX_VALUE)
 					return new Float((float) d);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		if (to == char.class) {
 			if (value instanceof Number) {
 				long l = number.longValue();
 				double d = number.doubleValue();
-				if (l == d && l >= Character.MIN_VALUE && l <= Character.MAX_VALUE)
+				if (l == d && l >= Character.MIN_VALUE
+						&& l <= Character.MAX_VALUE)
 					return new Character((char) l);
-				else return NOT_CONVERTIBLE;
-			}
-			else return NOT_CONVERTIBLE;
+				else
+					return NOT_CONVERTIBLE;
+			} else
+				return NOT_CONVERTIBLE;
 		}
 
 		return NOT_CONVERTIBLE;
@@ -344,16 +392,25 @@ public class InvokeService implements ICommandService {
 
 	@SuppressWarnings("rawtypes")
 	private static Class toPrimitive(Class boxed) {
-		if (boxed.isPrimitive()) return boxed;
+		if (boxed.isPrimitive())
+			return boxed;
 
-		if (boxed == Boolean.class) return boolean.class;
-		if (boxed == Character.class) return char.class;
-		if (boxed == Byte.class) return byte.class;
-		if (boxed == Short.class) return short.class;
-		if (boxed == Integer.class) return int.class;
-		if (boxed == Long.class) return long.class;
-		if (boxed == Float.class) return float.class;
-		if (boxed == Double.class) return double.class;
+		if (boxed == Boolean.class)
+			return boolean.class;
+		if (boxed == Character.class)
+			return char.class;
+		if (boxed == Byte.class)
+			return byte.class;
+		if (boxed == Short.class)
+			return short.class;
+		if (boxed == Integer.class)
+			return int.class;
+		if (boxed == Long.class)
+			return long.class;
+		if (boxed == Float.class)
+			return float.class;
+		if (boxed == Double.class)
+			return double.class;
 
 		return null;
 	}
