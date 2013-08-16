5d344fa196f5ae34e610684858c660560eada6ea
mikayel.nersisyan <mikayel.nersisyan@xored.com>
2013-08-16 19:24:34 +0700

    QS-2818: ECL Editor Improvements.
    Removed dependency of ECL from Q7.

--- plugins/org.eclipse.ecl.parser/src/org/eclipse/ecl/parser/EclValidator.java
+++ plugins/org.eclipse.ecl.parser/src/org/eclipse/ecl/parser/EclValidator.java
@@ -10,28 +10,14 @@
  ******************************************************************************/
 package org.eclipse.ecl.parser;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ecl.core.Command;
 import org.eclipse.ecl.core.Exec;
-import org.eclipse.ecl.core.util.DeclarationInfo;
-import org.eclipse.ecl.core.util.DeclarationInfo.DeclarationInfoType;
-import org.eclipse.ecl.internal.core.CorePlugin;
 import org.eclipse.ecl.runtime.EclCompiler;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
-
-import com.xored.q7.ecl.parser.EclViewerParser;
 
 public enum EclValidator {
 
@@ -47,56 +33,7 @@ public enum EclValidator {
 			return e.getStatus();
 		}
 		return Status.OK_STATUS;
-	}
-
-	public IStatus[] validateReferences(String content, Map<String, List<String>> mapIdToParameters, Map<String, String> mapIdToScripts) {
-		EclViewerParser parser = new EclViewerParser();
-		Map<String, List<DeclarationInfo>> mapOfDeclarations = parser.getAllDeclarations(content, mapIdToParameters, mapIdToScripts, false);
-
-		if (mapOfDeclarations == null)
-			return new IStatus[] { Status.OK_STATUS };
-
-		if (mapOfDeclarations.size() == 0)
-			return new IStatus[] { Status.OK_STATUS };
-
-		Iterator<Entry<String, List<DeclarationInfo>>> it = mapOfDeclarations.entrySet().iterator();
-		List<IStatus> statuses = new ArrayList<IStatus>();
-
-		while (it.hasNext()) {
-			Entry<String, List<DeclarationInfo>> entry = it.next();
-			List<DeclarationInfo> listOfDeclarationInfos = entry.getValue();
-
-			if (listOfDeclarationInfos == null)
-				continue;
-
-			int size = listOfDeclarationInfos.size();
-			for (int i = 0; i < size; ++i) {
-				DeclarationInfo declInfo = listOfDeclarationInfos.get(i);
-
-				if (size == 1) {
-					if (declInfo.getType() == DeclarationInfoType.NonExisting) {
-						statuses.add(new ScriptErrorStatus(IStatus.ERROR, CorePlugin.PLUGIN_ID, entry.getKey() + " cannot be resolved as refernece or procedure.", declInfo
-								.getDeclarationOffset(), 1, entry.getKey().length()));
-					}
-				} else {
-					String statusMessage = "Duplicate declaration of " + entry.getKey() + ".";
-					
-					if(declInfo.getType() == DeclarationInfoType.Parameter){
-						statusMessage = " Also declared as parameter.";
-					}
-					
-					statuses.add(new ScriptErrorStatus(IStatus.WARNING, CorePlugin.PLUGIN_ID, statusMessage, declInfo
-							.getDeclarationOffset(), 1, entry.getKey().length()));
-				}
-			}
-		}
-
-		if (statuses.size() > 0) {
-			return statuses.toArray(new IStatus[statuses.size()]);
-		}
-
-		return new IStatus[] { Status.OK_STATUS };
-	}
+	}	
 
 	private IStatus validate(Exec exec) throws CoreException {
 		Command command = EclCompiler.compile(exec);
