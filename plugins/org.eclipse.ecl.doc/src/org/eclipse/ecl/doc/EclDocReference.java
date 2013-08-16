87d6b1ee4882ec3d660ecfdb4167201717bb96e0
mikayel.nersisyan <mikayel.nersisyan@xored.com>
2013-08-16 15:48:45 +0700

    QS-2818: ECL Editor Improvements
    Sub tasks:
    
    QS-2819: Navigation to parameter in Parameters context
    QS-2821: Add tooltip information for references
    QS-2822: Mark duplicate declarations with error mark
    QS-2823: Mark non existing declarations with error mark
    QS-2824: Navigation to procedures
    QS-2830: Automatic completeion of references on $ sign
    QS-2831: Automatic completion of user defined procedures
    QS-2833: Add $ sign to autocompletion triggers
    QS-2834: Ignore all public commands for Open Declaration action.
    QS-2835: Fix markers in context editor and in main editor
    QS-2836: Mark occurrences in ecl editor part of context editor.

---
+++ plugins/org.eclipse.ecl.doc/src/org/eclipse/ecl/doc/EclDocReference.java
@@ -0,0 +1,89 @@
+package org.eclipse.ecl.doc;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.ecl.core.util.DeclarationInfo;
+
+
+
+public class EclDocReference implements IEclDocProvider {
+	List<DeclarationInfo> listOfDeclarations;
+	String refName;
+
+	public EclDocReference(String refName, List<DeclarationInfo> listOfDeclarations) {
+		this.listOfDeclarations = listOfDeclarations;
+		this.refName = refName;
+	}
+
+	public List<DeclarationInfo> getListOfDeclarations() {
+		return listOfDeclarations;
+	}
+
+	public void writeEclDoc(EclDocWriter writer) throws IOException {
+		if ((listOfDeclarations == null) || (listOfDeclarations.size() == 0)) {
+			writer.text("");
+			writer.b(refName);
+			writer.text(" cannot be resolved as reference or procedure.");
+		} else {			
+			writer.b(refName);
+			if (listOfDeclarations.size() > 1) {
+				writer.text(" Mutliple declarations have been found.");
+			}
+
+			for (DeclarationInfo declInfo : listOfDeclarations) {
+				writer.dl();
+				{
+					switch (declInfo.getType()) {
+					case Parameter: {
+						writer.dt("Defined in: ");
+						writer.dd("Parameters Context.");
+					}
+						break;
+					case File: {
+						writer.dt("Defined in: ");
+						if (declInfo.getQ7NamedElementID().isEmpty()) {
+							writer.dd(" current script at <b>line " + declInfo.getDeclarationOffset() + "</b> as \"<b>" + declInfo.getDeclarationString()
+									+ "</b>\"");
+						} else {
+							writer.dd(" another file at <b>line " + declInfo.getDeclarationOffset() + "</b> as \"<b>" + declInfo.getDeclarationString()
+									+ "<b>\"");
+						}
+
+					}
+						break;
+					case NonExisting: {
+						writer.dd("Cannot be resolved as reference or procedure.");
+					}
+						break;
+					}
+				}
+				writer.close();
+			}
+		}
+		/*
+		 * if ((listOfDeclarations == null) || (listOfDeclarations.size() == 0))
+		 * { writer.text("Reference: "); writer.b(refName);
+		 * writer.text(" is undefined."); } else { writer.text("Reference: ");
+		 * writer.b(refName); writer.text(".");
+		 * 
+		 * if (listOfDeclarations.size() > 1) {
+		 * writer.text(" Mutliple declarations have been found."); }
+		 * 
+		 * for (DeclarationInfo declInfo : listOfDeclarations) { writer.dl(); {
+		 * writer.dt("Defined in: "); switch (declInfo.getType()) { case
+		 * Parameter: { writer.dd("Parameters Context."); } break; case File: {
+		 * if (declInfo.getQ7NamedElementID().isEmpty()) {
+		 * writer.dd(" current script at <b>line " +
+		 * declInfo.getDeclarationOffset() + "</b> as \"<b>" +
+		 * declInfo.getDeclarationString() + "</b>\""); } else {
+		 * writer.dd(" another file at <b>line " +
+		 * declInfo.getDeclarationOffset() + "</b> as \"<b>" +
+		 * declInfo.getDeclarationString() + "<b>\""); }
+		 * 
+		 * } break; case NonExisting: { writer.dd("<b>Undefined.</b>"); } break;
+		 * } } writer.close(); } }
+		 */
+	}
+
+}
