8bcec7dbe2f4e54eebefa0e5f38f9b7040468470
gulevich <vasili.gulevich@xored.com>
2014-05-29 13:08:41 +0700

    QS-3233 Get extension for file refactored to use enum keys.

--- plugins/org.eclipse.ecl.filesystem/src/org/eclipse/ecl/filesystem/internal/commands/GetFromFileService.java
+++ plugins/org.eclipse.ecl.filesystem/src/org/eclipse/ecl/filesystem/internal/commands/GetFromFileService.java
@@ -1,6 +1,5 @@
 package org.eclipse.ecl.filesystem.internal.commands;
 
-import static java.util.Arrays.asList;
 import static org.eclipse.ecl.filesystem.EclFilesystemPlugin.createError;
 import static org.eclipse.ecl.runtime.BoxedValues.unbox;
 
@@ -8,9 +7,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.URIUtil;
@@ -21,14 +18,33 @@ import org.eclipse.ecl.filesystem.File;
 import org.eclipse.ecl.filesystem.FilesystemFactory;
 import org.eclipse.ecl.runtime.SingleCommandService;
 
-public class GetFromFileService extends SingleCommandService<Get> implements IScriptletExtension {
+public class GetFromFileService extends SingleCommandService<Get> implements
+		IScriptletExtension {
 
 	public GetFromFileService() {
 		super(Get.class);
 	}
 
-	private final static Set<String> SUPPORTED_KEYS = Collections.unmodifiableSet(new HashSet<String>(asList("exists", "children", "name", "isDirectory")));
-	
+	enum Key {
+		EXISTS, CHILDREN, NAME, ISDIRECTORY
+	}
+
+	static private Key parseKey(Object object) {
+		Object keyObject = unbox(object);
+		if (keyObject == null)
+			return null;
+		if (!(keyObject instanceof String))
+			return null;
+		String keyString = (String) keyObject;
+		try {
+			// TODO: implement a method that doesn't use exceptions to indicate
+			// invalid strings
+			return Key.valueOf(Key.class, keyString.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			return null;
+		}
+	}
+
 	@Override
 	public boolean canHandle(Command command) {
 		if (!(command instanceof Get))
@@ -36,48 +52,51 @@ public class GetFromFileService extends SingleCommandService<Get> implements ISc
 		Get get = (Get) command;
 		if (!(get.getInput() instanceof File))
 			return false;
-		return SUPPORTED_KEYS.contains(unbox(get.getKey()));
+		return parseKey(get.getKey()) != null;
 	}
 
 	@Override
 	protected Object serviceTyped(Get command) throws InterruptedException,
 			CoreException {
-		String key = (String) unbox(command.getKey());
-		String uriString = ((File)command.getInput()).getUri();
-		URI input = URI.create(uriString);
-		if (key.equals("exists"))
-			return handleExists(input);
-		if (key.equals("children")) {
-			try {
-				return handleChildren(input);
-			} catch (MalformedURLException e) {
-				throw new CoreException(createError("Can't create child URI", e));
-			}
+		Key key = parseKey(command.getKey());
+		if (key == null) {
+			throw new CoreException(createError("Invalid key: "
+					+ command.getKey()));
 		}
-		if (key.equals("name"))
-			return handleName(input);
-		if (key.equals("isDirectory"))
+		String uriString = ((File) command.getInput()).getUri();
+		URI input = URI.create(uriString);
+		switch (key) {
+		case CHILDREN:
+			return handleChildren(input);
+		case EXISTS:
+			return handleExists(input);
+		case ISDIRECTORY:
 			return handleIsDirectory(input);
-		String message = "Wrong key: " + key;
-		throw new CoreException(createError(message));
+		case NAME:
+			return handleName(input);
+		}
+		throw new CoreException(createError("Wrong key: " + key));
 	}
-
 
 	private boolean handleIsDirectory(URI input) {
 		return URIUtil.toFile(input).isDirectory();
 	}
 
-	private List<File> handleChildren(URI input) throws MalformedURLException {
-		java.io.File[] files = URIUtil.toFile(input).listFiles();
-		if (files == null)
-			return Collections.emptyList(); 
-		ArrayList<File> rv = new ArrayList<File>();
-		for (java.io.File child: files) {
-			File item = FilesystemFactory.eINSTANCE.createFile();
-			item.setUri(child.toURI().toURL().toExternalForm());
-			rv.add(item);
+	private List<File> handleChildren(URI input) throws CoreException {
+		try {
+			java.io.File[] files = URIUtil.toFile(input).listFiles();
+			if (files == null)
+				return Collections.emptyList();
+			ArrayList<File> rv = new ArrayList<File>();
+			for (java.io.File child : files) {
+				File item = FilesystemFactory.eINSTANCE.createFile();
+				item.setUri(child.toURI().toURL().toExternalForm());
+				rv.add(item);
+			}
+			return rv;
+		} catch (MalformedURLException e) {
+			throw new CoreException(createError("Can't create child URI", e));
 		}
-		return rv;
 	}
 
 	private Object handleName(URI uri) {
